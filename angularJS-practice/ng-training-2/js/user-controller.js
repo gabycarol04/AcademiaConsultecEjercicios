22656f3ed523ac935b3369bae3cd3375288eafa8
gabycarol04 <gabycarol90@gmail.com>
2021-07-04 15:09:01 -0500

    probando carpeta

--- angularJS-practice/ng-training-2/js/user-controller.js
+++
@@ -1,9 +0,0 @@
-/* app.controller('UserController', ['$scope', function($scope){
-    $scope.aula = "Las Tablas";
-}]); */
-app.controller('UserController', function($scope){
-
-    //Parte 1: con Scope
-    $scope.aula = "Programadores";
-    $scope.languages = ['HTML', 'JS', 'TS', 'PYTHON', 'PHP'];
-});
